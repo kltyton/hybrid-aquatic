ae4af014441faf5d68a91d6c7e495a0d18f870f7
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2024-02-25 16:24:42 +0300

    and this

--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/armor/DivingArmorRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/armor/DivingArmorRenderer.kt
@@ -1,13 +1,11 @@
-package dev.hybridlabs.aquatic.client.render.armor;
+package dev.hybridlabs.aquatic.client.render.armor
 
-import dev.hybridlabs.aquatic.HybridAquatic;
-import dev.hybridlabs.aquatic.item.DivingArmorItem;
-import net.minecraft.util.Identifier;
-import software.bernie.geckolib.model.DefaultedItemGeoModel;
-import software.bernie.geckolib.renderer.GeoArmorRenderer;
+import dev.hybridlabs.aquatic.HybridAquatic
+import dev.hybridlabs.aquatic.item.DivingArmorItem
+import net.minecraft.util.Identifier
+import software.bernie.geckolib.model.DefaultedItemGeoModel
+import software.bernie.geckolib.renderer.GeoArmorRenderer
 
-public final class DivingArmorRenderer extends GeoArmorRenderer<DivingArmorItem> {
-	public DivingArmorRenderer() {
-		super(new DefaultedItemGeoModel<>(new Identifier(HybridAquatic.MOD_ID, "armor/diving_armor")));
-	}
-}
+class DivingArmorRenderer: GeoArmorRenderer<DivingArmorItem>(
+    DefaultedItemGeoModel(Identifier(HybridAquatic.MOD_ID, "armor/diving_armor"))
+)
