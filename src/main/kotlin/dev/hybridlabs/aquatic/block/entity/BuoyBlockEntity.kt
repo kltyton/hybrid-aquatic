3c41c903a2726bc097bf3288a03d46b66778cbe6
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-06-09 05:55:58 +0300

    Buoys no longer give clarity

--- src/main/kotlin/dev/hybridlabs/aquatic/block/entity/BuoyBlockEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/block/entity/BuoyBlockEntity.kt
@@ -1,13 +1,8 @@
 package dev.hybridlabs.aquatic.block.entity
 
-import dev.hybridlabs.aquatic.effect.HybridAquaticStatusEffects
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntity
-import net.minecraft.entity.effect.StatusEffectInstance
-import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.Box
-import net.minecraft.world.World
 import software.bernie.geckolib.core.animatable.GeoAnimatable
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache
 import software.bernie.geckolib.core.animation.*
@@ -47,29 +42,7 @@ class BuoyBlockEntity(pos: BlockPos, state: BlockState) : BlockEntity(HybridAqua
         return RenderUtils.getCurrentTick()
     }
 
-    private fun givePlayersEffects(world: World, pos: BlockPos) {
-        val j = 12
-
-        val box = Box(
-            pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble(),
-            (pos.x + 1).toDouble(), (pos.y + 1).toDouble(), (pos.z + 1).toDouble()
-        ).expand(j.toDouble()).stretch(0.0, world.height.toDouble(), 0.0)
-
-        val list = world.getNonSpectatingEntities(PlayerEntity::class.java, box)
-        if (list.isNotEmpty()) {
-            for (entity in list) {
-                if (pos.isWithinDistance(entity.blockPos, j.toDouble())) {
-                    entity.addStatusEffect(StatusEffectInstance(HybridAquaticStatusEffects.CLARITY, 200, 0, true, false))
-                }
-            }
-        }
-    }
-
     companion object {
         val FLOAT_ANIMATION: RawAnimation = RawAnimation.begin().then("float", Animation.LoopType.LOOP)
-
-        fun tick(world: World, pos: BlockPos, state: BlockState, blockEntity: BuoyBlockEntity) {
-            blockEntity.givePlayersEffects(world, pos)
-        }
     }
 }
