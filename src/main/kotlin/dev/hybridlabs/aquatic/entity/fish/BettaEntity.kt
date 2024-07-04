035407c081f165f946890c09848d2ac2156ecf11
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-07-04 15:30:43 +0300

    Fixed betta variants

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/BettaEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/BettaEntity.kt
@@ -1,5 +1,6 @@
 package dev.hybridlabs.aquatic.entity.fish
 
+import dev.hybridlabs.aquatic.tag.HybridAquaticBiomeTags
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.attribute.DefaultAttributeContainer
@@ -8,7 +9,15 @@ import net.minecraft.entity.mob.WaterCreatureEntity
 import net.minecraft.world.World
 
 class BettaEntity(entityType: EntityType<out BettaEntity>, world: World) :
-    HybridAquaticFishEntity(entityType, world, emptyMap(), HybridAquaticEntityTags.NONE, HybridAquaticEntityTags.NONE) {
+    HybridAquaticFishEntity(entityType, world, variants = hashMapOf(
+        "black" to FishVariant.biomeVariant("black", HybridAquaticBiomeTags.BETTA_SPAWN_BIOMES),
+        "blue" to FishVariant.biomeVariant("blue", HybridAquaticBiomeTags.BETTA_SPAWN_BIOMES),
+        "blue_yellow" to FishVariant.biomeVariant("blue_yellow", HybridAquaticBiomeTags.BETTA_SPAWN_BIOMES),
+        "green" to FishVariant.biomeVariant("green", HybridAquaticBiomeTags.BETTA_SPAWN_BIOMES),
+        "pink" to FishVariant.biomeVariant("pink", HybridAquaticBiomeTags.BETTA_SPAWN_BIOMES),
+        "red" to FishVariant.biomeVariant("red", HybridAquaticBiomeTags.BETTA_SPAWN_BIOMES),
+        "white" to FishVariant.biomeVariant("white", HybridAquaticBiomeTags.BETTA_SPAWN_BIOMES),),
+        HybridAquaticEntityTags.NONE, HybridAquaticEntityTags.NONE) {
     companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
