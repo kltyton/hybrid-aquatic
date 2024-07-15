b7ca172954a7c68bdef88302454186acbf35c177
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-07-15 09:31:29 +0300

    Small parrotfish fix

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/ParrotfishEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/ParrotfishEntity.kt
@@ -6,34 +6,12 @@ import net.minecraft.entity.EntityType
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.mob.WaterCreatureEntity
-import net.minecraft.util.Identifier
 import net.minecraft.world.World
 
 class ParrotfishEntity(entityType: EntityType<out ParrotfishEntity>, world: World) :
     HybridAquaticFishEntity(entityType, world, variants = hashMapOf(
         "humphead" to FishVariant.biomeVariant("humphead", HybridAquaticBiomeTags.PARROTFISH_SPAWN_BIOMES),),
         HybridAquaticEntityTags.NONE, HybridAquaticEntityTags.NONE) {
-
-    public override fun getLootTableId(): Identifier {
-        return when (this.variant?.variantName) {
-            "blue_tang" -> Identifier("hybrid-aquatic", "gameplay/blue_tang")
-            "sohal" -> Identifier("hybrid-aquatic", "gameplay/sohal")
-            "orangeshoulder" -> Identifier("hybrid-aquatic", "gameplay/orangeshoulder")
-            "unicornfish" -> Identifier("hybrid-aquatic", "gameplay/unicornfish")
-            else -> super.getLootTableId()
-        }
-    }
-
-    override fun tick() {
-        super.tick()
-
-        return when (this.variant?.variantName) {
-            "powder_blue_tang" -> attributes.getCustomInstance(EntityAttributes.GENERIC_MAX_HEALTH)?.baseValue = 1.5
-            "yellow_tang" -> attributes.getCustomInstance(EntityAttributes.GENERIC_MAX_HEALTH)?.baseValue = 1.5
-
-            else -> attributes.getCustomInstance(EntityAttributes.GENERIC_MAX_HEALTH)?.baseValue = 4.0
-        }
-    }
 
     override fun getLimitPerChunk(): Int {
         return 4
