5c20c9bda763a30bff213a0e96f250884887cdf0
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-07-17 13:01:28 +0300

    Buffed buoyancy effect

--- src/main/kotlin/dev/hybridlabs/aquatic/effect/Buoyancy.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/effect/Buoyancy.kt
@@ -8,13 +8,13 @@ import net.minecraft.util.math.Vec3d
 class Buoyancy : StatusEffect(StatusEffectCategory.BENEFICIAL, 0xFFFFFF) {
 
     override fun canApplyUpdateEffect(duration: Int, amplifier: Int): Boolean {
-        return duration % 40 == 0
+        return duration % 20 == 0
     }
 
     override fun applyUpdateEffect(entity: LivingEntity, amplifier: Int) {
         val currentVelocity = entity.velocity
         if (entity.isSubmergedInWater) {
-            entity.velocity = Vec3d(currentVelocity.x, 0.25, currentVelocity.z)
+            entity.velocity = Vec3d(currentVelocity.x, 1.0, currentVelocity.z)
         }
     }
 }
