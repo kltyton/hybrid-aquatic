f2d702d3e459a762131503918c3f74e0d5826b25
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-07-25 16:12:40 +0300

    Fixed.. something?

---
+++ remappedSrc/dev/hybridlabs/aquatic/access/CustomFishingBobberEntityData.java
@@ -0,0 +1,13 @@
+package dev.hybridlabs.aquatic.access;
+
+import net.minecraft.item.ItemStack;
+
+public interface CustomFishingBobberEntityData {
+  default void hybrid_aquatic$setLureItem(ItemStack value) {
+    throw new AssertionError();
+  }
+  
+  default ItemStack hybrid_aquatic$getLureItem() {
+    throw new AssertionError();
+  }
+}
