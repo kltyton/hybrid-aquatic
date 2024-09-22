35f68cf396fcd568cecd561a76e027f679c8abc1
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-22 12:52:38 -0300

    Update ThresherSharkEntity.kt removed armor toughness

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/ThresherSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/ThresherSharkEntity.kt
@@ -2,17 +2,14 @@ package dev.hybridlabs.aquatic.entity.shark
 
 import dev.hybridlabs.aquatic.effect.HybridAquaticStatusEffects
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
-import dev.hybridlabs.aquatic.tag.HybridAquaticItemTags
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.ai.goal.RevengeGoal
-import net.minecraft.entity.ai.goal.TemptGoal
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.mob.WaterCreatureEntity
-import net.minecraft.recipe.Ingredient
 import net.minecraft.sound.SoundEvents
 import net.minecraft.world.Difficulty
 import net.minecraft.world.World
@@ -20,30 +17,25 @@ import net.minecraft.world.World
 class ThresherSharkEntity(entityType: EntityType<out ThresherSharkEntity>, world: World) :
     HybridAquaticSharkEntity(entityType, world, HybridAquaticEntityTags.THRESHER_SHARK_PREY, false, false) {
 
-    override fun getLimitPerChunk(): Int {
-        return 1
-    }
-
     override fun initGoals() {
         super.initGoals()
         goalSelector.add(1, RevengeGoal(this))
-        goalSelector.add(1, TemptGoal(this, 0.8, Ingredient.fromTag(HybridAquaticItemTags.RAW_FISHES), false))
     }
 
         companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 24.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.8)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 36.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.8)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 16.0)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
         }
     }
 
     override fun tryAttack(target: Entity?): Boolean {
         if (super.tryAttack(target)) {
 
-            playSound(SoundEvents.ENTITY_FOX_BITE,2.5F,0.0F)
+            playSound(SoundEvents.ENTITY_FOX_BITE,5.0F,0.0F)
 
             if (target is LivingEntity) {
                 var i = 0
