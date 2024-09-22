73b991325247dfb83665c7aef753c6591268d198
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-22 12:51:10 -0300

    Update BaskingSharkEntity.kt Removed Armor Toughness

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/BaskingSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/BaskingSharkEntity.kt
@@ -1,35 +1,21 @@
 package dev.hybridlabs.aquatic.entity.shark
 
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
-import dev.hybridlabs.aquatic.tag.HybridAquaticItemTags
 import net.minecraft.entity.EntityType
-import net.minecraft.entity.ai.goal.RevengeGoal
-import net.minecraft.entity.ai.goal.TemptGoal
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.mob.WaterCreatureEntity
-import net.minecraft.recipe.Ingredient
 import net.minecraft.world.World
 
 class BaskingSharkEntity(entityType: EntityType<out BaskingSharkEntity>, world: World) :
     HybridAquaticSharkEntity(entityType, world, HybridAquaticEntityTags.NONE, true, false) {
-
-    override fun getLimitPerChunk(): Int {
-        return 1
-    }
-
-    override fun initGoals() {
-        super.initGoals()
-        goalSelector.add(1, TemptGoal(this, 0.8, Ingredient.fromTag(HybridAquaticItemTags.RAW_FISHES), false))
-    }
-
     companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 40.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.8)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.0)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 3.0)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 16.0)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
                 .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 100.0)
         }
     }
