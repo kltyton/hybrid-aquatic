89d2bcf87dd9e89fb69c041d56db11977fde9aa5
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-22 12:53:10 -0300

    Update WhaleSharkEntity.kt removed armor toughness

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/WhaleSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/WhaleSharkEntity.kt
@@ -1,37 +1,24 @@
 package dev.hybridlabs.aquatic.entity.shark
 
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
-import dev.hybridlabs.aquatic.tag.HybridAquaticItemTags
 import net.minecraft.entity.EntityType
-import net.minecraft.entity.ai.goal.TemptGoal
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.mob.WaterCreatureEntity
-import net.minecraft.recipe.Ingredient
 import net.minecraft.world.World
 
 class WhaleSharkEntity(entityType: EntityType<out WhaleSharkEntity>, world: World) :
     HybridAquaticSharkEntity(entityType, world, HybridAquaticEntityTags.NONE, true, false) {
-
-    override fun getLimitPerChunk(): Int {
-        return 1
-    }
-
-    override fun initGoals() {
-        super.initGoals()
-        goalSelector.add(1, TemptGoal(this, 0.8, Ingredient.fromTag(HybridAquaticItemTags.RAW_FISHES), false))
-    }
-
-    companion object {
-        fun createMobAttributes(): DefaultAttributeContainer.Builder {
-            return WaterCreatureEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 40.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.8)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 16.0)
-                .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 100.0)
+        companion object {
+            fun createMobAttributes(): DefaultAttributeContainer.Builder {
+                return WaterCreatureEntity.createMobAttributes()
+                    .add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0)
+                    .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.0)
+                    .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0)
+                    .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
+                    .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 100.0)
+            }
         }
-    }
 
     override fun getMaxSize(): Int {
         return 5
