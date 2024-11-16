4b5bb9544e28dd1be41822fa98883c9761b3bb95
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-11-17 01:14:01 +0200

    Adjusted Lionfish speed

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/LionfishEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/LionfishEntity.kt
@@ -30,7 +30,7 @@ class LionfishEntity(entityType: EntityType<out LionfishEntity>, world: World) :
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 4.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.7)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.6)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 12.0)
         }
