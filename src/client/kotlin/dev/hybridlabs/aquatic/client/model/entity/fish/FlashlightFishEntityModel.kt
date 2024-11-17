97f14fadd7e9fbe25b748c8ce2c2bf6716122e76
fufufuns <139853036+kltyton@users.noreply.github.com>
2024-11-17 12:45:34 +0800

    Update FlashlightFishEntityModel.kt
    Fixed a deadlock issue where it was loaded at the same time as the physics mod

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/FlashlightFishEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/FlashlightFishEntityModel.kt
@@ -3,18 +3,25 @@ package dev.hybridlabs.aquatic.client.model.entity.fish
 import dev.hybridlabs.aquatic.HybridAquatic
 import dev.hybridlabs.aquatic.entity.fish.FlashlightFishEntity
 import net.minecraft.util.Identifier
+import java.util.concurrent.CompletableFuture
 
 class FlashlightFishEntityModel : HybridAquaticFishEntityModel<FlashlightFishEntity>("flashlight_fish") {
+
+    private val onTextureFuture: CompletableFuture<Identifier> = CompletableFuture.supplyAsync {
+        Identifier(HybridAquatic.MOD_ID, "textures/entity/fish/flashlight_fish/flashlight_fish.png")
+    }
+
+    private val offTextureFuture: CompletableFuture<Identifier> = CompletableFuture.supplyAsync {
+        Identifier(HybridAquatic.MOD_ID, "textures/entity/fish/flashlight_fish/flashlight_fish_off.png")
+    }
 
     override fun getTextureResource(animatable: FlashlightFishEntity?): Identifier {
         val isLightOn = animatable?.isLightOn ?: true
 
-        val texturePath = if (isLightOn) {
-            "textures/entity/fish/flashlight_fish/flashlight_fish.png"
+        return if (isLightOn) {
+            onTextureFuture.get()
         } else {
-            "textures/entity/fish/flashlight_fish/flashlight_fish_off.png"
+            offTextureFuture.get()
         }
-
-        return Identifier(HybridAquatic.MOD_ID, texturePath)
     }
 }
